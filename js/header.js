6bbfc11cf7fafe19edf58139d070e365eede07b6
spoorthyv <sckortyman@gmail.com>
2016-10-17 14:39:27 -0700

    Add jupyter stuff and make header smaller on scroll

--- js/header.js
+++ js/header.js
@@ -3,3 +3,18 @@ jQuery(function($){
      	$('.mobile-menu').toggleClass('expand')
      })
 })
+
+jQuery(function($){
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 100,
+            header = $(".header");
+        if (distanceY > shrinkOn && $(window).width() > 480) {
+            header.addClass("scroll");
+        } else {
+            if (header.hasClass("scroll")) {
+              header.removeClass("scroll")
+            }
+        }
+    });
+})
