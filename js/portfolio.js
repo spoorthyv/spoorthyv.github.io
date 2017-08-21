b292ebf57d4d260642b3658aeea69fde41351b0d
spoorthyv <sckortyman@gmail.com>
2017-08-21 00:54:58 -0700

    v1 of middleman site

--- js/portfolio.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($){
-   var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-   if (width < 960) {
-     document.getElementById("currentLabel").innerHTML = "Current Projects";
-   }
-})
---
+++ source/javascripts/site.js
@@ -0,0 +1,5 @@
+// This is where it all goes :)
+$('.navbar a').hover(
+   function(){ $(this).addClass('active') },
+   function(){ $(this).removeClass('active') }
+)
