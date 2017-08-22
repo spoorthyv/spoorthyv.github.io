59e0bede80133d3e19a3d6bada7afcda77c9adf9
spoorthyv <sckortyman@gmail.com>
2017-08-21 23:15:44 -0700

    Remove unnessary files and add page transitions

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,36 @@
 //= require jquery
 //= require turbolinks
 
-console.log("Hi");
-$("body");
+Turbolinks.start();
+
+if ( 'querySelector' in document && 'addEventListener' in window ) {
+
+
+   
+  // if (document.documentElement.hasAttribute("data-turbolinks-preview")) {
+  //   var wrapper = document.getElementById('wrapper');
+  //   removeClass(wrapper, 'fadeInDown');
+  //   addClass(wrapper, 'fadeOutUp');
+  // }
+
+   document.addEventListener("turbolinks:click", function(e) {
+      var wrapper = $('#wrapper');
+      wrapper.removeClass('fadeInDown');
+      wrapper.addClass('fadeOutUp');
+   }, false);
+
+   document.addEventListener("turbolinks:load", function(e) {
+      var wrapper = $('#wrapper');
+      wrapper.removeClass('fadeOutUp');
+      wrapper.addClass('fadeInDown');
+   }, false);
+
+  document.addEventListener("turbolinks:before-cache", function() {
+   var wrapper = $('#wrapper');
+   wrapper.removeClass('fadeOutUp');
+   wrapper.addClass('fadeInDown');
+  }, false);
+
+}
+
+
--- source/javascripts/site.js
+++
@@ -1,5 +0,0 @@
-// This is where it all goes :)
-$('.navbar a').hover(
-   function(){ $(this).addClass('active') },
-   function(){ $(this).removeClass('active') }
-)
