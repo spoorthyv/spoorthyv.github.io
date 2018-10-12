954d1c6c4cd7316c7f249ede93053fbb78a7c695
spoorthyv <sckortyman@gmail.com>
2018-10-11 17:49:45 -0700

    Attempt 1 to redirect http to https

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,11 @@
 //= require jquery
 //= require turbolinks
 
+var host = "www.spoorthyv.com"
+if (window.location.host == host && window.location.protocol != "https:") {
+  window.location.protocol = "https:"
+}
+
 Turbolinks.start();
 
 var isMain = "first";
