b3ea1ad7599d06cf030467bddb18de05e3e03836
Spoorthy Vemula <spoorthyvemula@gmail.com>
2021-02-05 22:27:02 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9631da14386bc89570bc2812d169b12.js"
+  "/precache-manifest.436674cf4a12c3acfab3f0eb3f7871e5.js"
 );
 
 self.addEventListener('message', (event) => {
